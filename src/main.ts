5ffe33f9b8459462455a203bec906144d60fcf79
saugat1070 <saugatgiri1070@gmail.com>
2026-01-15 19:38:41 +0545

    main

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 export const app = express();
 app.use(express.json());
 app.use(cors({
-    origin : "*"
+    origin : "*",
+    credentials : true
 }))
 connectDb(); // Database Connection
