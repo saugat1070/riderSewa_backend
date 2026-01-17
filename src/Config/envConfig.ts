e4d58419fbe91d1f020e714c0eeafa34d42b4cea
saugat1070 <saugatgiri1070@gmail.com>
2026-01-17 21:35:35 +0545

    configure files

--- src/Config/envConfig.ts
+++ src/Config/envConfig.ts
@@ -2,13 +2,15 @@ process.loadEnvFile(".env")
 interface EnvConfig {
     portNumber:number;
     dbUrl : string;
-    nodeEnv : string
+    nodeEnv : string;
+    jwtSecret : string;
 }
 
 const Env : EnvConfig = {
     portNumber : Number(process.env.PORT),
     dbUrl : process.env.DB_URL as string,
-    nodeEnv : process.env.NODE_ENV as string
+    nodeEnv : process.env.NODE_ENV as string,
+    jwtSecret : process.env.JWT_SECRET_KEY as string
 }
 
 export default Env;
