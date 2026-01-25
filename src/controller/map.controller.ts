a86ec44cc902d17e381fe6d5597541217f2d298d
saugat1070 <saugatgiri1070@gmail.com>
2026-01-25 14:53:09 +0545

    getDistance and getautosuggestion complete

--- src/controller/map.controller.ts
+++ src/controller/map.controller.ts
@@ -1,6 +1,7 @@
 import { Request,Response } from "express"
 import {validationResult} from "express-validator"
 import MapService from "../Services/map.service.js";
+import { GetCoordinateDto,GetDistanceTimeDto } from "../DTO/map.dto.js";
 
 export const getCoordinate = async (req:Request,res:Response)=>{
     const errors = validationResult(req);
@@ -9,7 +10,7 @@ export const getCoordinate = async (req:Request,res:Response)=>{
             error : errors.array()
         })
     }
-    const {address} = req.body;
+    const {address}:GetCoordinateDto = req.body;
     try {
         const coordinate = await MapService.getAddress(String(address));
         return res.json({res:coordinate})
@@ -19,4 +20,32 @@ export const getCoordinate = async (req:Request,res:Response)=>{
             error : error.message
         })
     }
+}
+
+export const getDistanceTime = async(req: Request, res: Response)=>{
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({
+            error : error.array()
+        });
+    }
+    const { origin, destination } = req.query as unknown as GetDistanceTimeDto;
+    if(!origin || !destination){
+        return res.status(400).json({message:"Origin and Destination must be provided"});
+    }
+    const responseDistanceTime = await MapService.getDistanceTime(origin,destination);
+    return res.status(200).json({message:"Success Distance and Time calculation",
+        result : responseDistanceTime
+    })
+}
+
+export const getAutoSuggestion = async (req:Request,res:Response)=>{
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({error:error.array()});
+    }
+
+    const {address} = req.query as unknown as GetCoordinateDto;
+    const responseAutoSuggestion = await MapService.getAutoCompleteSuggestion(address);
+    return res.status(200).json({autosuggestion:responseAutoSuggestion})
 }
