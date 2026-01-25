69f88d3f143b03b26965998e8106178cb5103aa4
saugat1070 <saugatgiri1070@gmail.com>
2026-01-25 14:51:04 +0545

    user related routes

--- src/Routes/user.routes.ts
+++ src/Routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import {validateToken} from "../middleware/auth.middleware.js"
+import { profile,loginUser,registerUser } from "../controller/user.controller.js";
 
 const userRoutes = Router();
 
-userRoutes.route("/create").post();
-userRoutes.route("/login").post();
-userRoutes.route("/").get();
+userRoutes.route("/create").post(registerUser);
+userRoutes.route("/login").post(loginUser);
+userRoutes.route("/").get(validateToken,profile);
+
+
+export default userRoutes;
